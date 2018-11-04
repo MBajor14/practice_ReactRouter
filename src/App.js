71b65e57b5057b21afd02e2f439aa42a2e784eb9
Mike Bajor <michael.bajor@gmail.com>
2018-11-04 10:39:17 -0500

    Set up SASS for cleaner css management

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MainSection from './components/smart/MainSection'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './css/styles.css';
 
 class App extends Component {
   render() {
